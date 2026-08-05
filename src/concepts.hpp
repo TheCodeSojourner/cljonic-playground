567ec8665e3a56a7cf3f6fd8bc2badf72fe461dc
Paul Whittington <pwhittington@ricelake.com>
2026-08-05 15:08:00 -0600

    Add concepts for VectorElement and NothrowCopyAssignable

---
+++ src/concepts.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <concepts>
+
+namespace cljonic::concepts
+{
+
+    template <typename value_type>
+    concept VectorElement = std::default_initializable<value_type> && std::copyable<value_type>;
+
+    template <typename value_type>
+    concept NothrowCopyAssignable = requires(value_type &target, const value_type &source) {
+        { target = source } noexcept -> std::same_as<value_type &>;
+    };
+
+} // namespace cljonic::concepts
