193d20e778d95e290fa46ee23e13e982089d0a33
Paul Whittington <pwhittington@ricelake.com>
2026-08-07 09:27:35 -0600

    Refactor Vector class and add collection maximum element count method

--- src/vector.hpp
+++ src/vector.hpp
@@ -6,85 +6,118 @@
 
 #include <concepts.hpp>
 
-namespace cljonic {
+#if !defined(CLJONIC_COLLECTION_MAXIMUM_ELEMENT_COUNT)
+#define CLJONIC_COLLECTION_MAXIMUM_ELEMENT_COUNT 1000
+#endif
 
-enum class vector_state {
-  empty,
-  populated,
-  at_capacity,
-};
+#define CLJONIC_STRINGIFY_INNER(x) #x
+#define CLJONIC_STRINGIFY(x) CLJONIC_STRINGIFY_INNER(x)
 
-namespace detail::vector {
-struct vector_observation {
-  int capacity_limit;
-  int logical_size;
-};
+namespace cljonic
+{
 
-[[nodiscard]] constexpr auto
-classify_vector(vector_observation observation) noexcept -> vector_state {
-  if (observation.logical_size == 0) {
-    return vector_state::empty;
-  }
+  enum class vector_state
+  {
+    empty,
+    populated,
+    at_capacity,
+  };
 
-  if (observation.logical_size == observation.capacity_limit) {
-    return vector_state::at_capacity;
-  }
+  namespace detail::vector
+  {
+    struct vector_observation
+    {
+      int capacity_limit;
+      int logical_size;
+    };
 
-  return vector_state::populated;
-}
-} // namespace detail::vector
+    [[nodiscard]] constexpr auto
+    classify_vector(vector_observation observation) noexcept -> vector_state
+    {
+      if (observation.logical_size == 0)
+      {
+        return vector_state::empty;
+      }
 
-template <concepts::VectorElement element_type, std::size_t capacity_value>
-class Vector {
-public:
-  using value_type = element_type;
+      if (observation.logical_size == observation.capacity_limit)
+      {
+        return vector_state::at_capacity;
+      }
 
-  template <typename... Args>
-    requires(sizeof...(Args) <= capacity_value) &&
-                (std::is_convertible_v<Args, element_type> && ...)
-  constexpr Vector(Args... args) noexcept(
-      (std::is_nothrow_constructible_v<element_type, Args> && ...))
-      : storage_{static_cast<element_type>(args)...},
-        logical_size_{sizeof...(Args)} {}
+      return vector_state::populated;
+    }
+  } // namespace detail::vector
 
-  [[nodiscard]] static constexpr auto capacity_limit() noexcept -> std::size_t {
-    return capacity_value;
-  }
+  template <concepts::VectorElement element_type, std::size_t capacity_value>
+  class Vector
+  {
+  public:
+    using value_type = element_type;
 
-  [[nodiscard]] constexpr auto logical_size() const noexcept -> std::size_t {
-    return logical_size_;
-  }
+    static_assert(
+        capacity_value <= CLJONIC_COLLECTION_MAXIMUM_ELEMENT_COUNT,
+        "Vector capacity exceeds CLJONIC_COLLECTION_MAXIMUM_ELEMENT_COUNT=" CLJONIC_STRINGIFY(CLJONIC_COLLECTION_MAXIMUM_ELEMENT_COUNT));
 
-  [[nodiscard]] constexpr auto state() const noexcept -> vector_state {
-    const detail::vector::vector_observation observation{
-        static_cast<int>(capacity_value), static_cast<int>(logical_size_)};
-    return detail::vector::classify_vector(observation);
-  }
-
-  [[nodiscard]] constexpr auto try_push_back(const value_type &value) noexcept(
-      concepts::NothrowCopyAssignable<value_type>) -> bool {
-    if (logical_size_ >= capacity_value) {
-      return false;
+    template <typename... Args>
+      requires(sizeof...(Args) <= capacity_value) &&
+                  (std::is_convertible_v<Args, element_type> && ...)
+    constexpr Vector(Args... args) noexcept(
+        (std::is_nothrow_constructible_v<element_type, Args> && ...))
+        : storage_{static_cast<element_type>(args)...},
+          logical_size_{sizeof...(Args)}
+    {
     }
 
-    storage_[logical_size_] = value;
-    ++logical_size_;
-    return true;
+    [[nodiscard]] static constexpr auto capacity_limit() noexcept -> std::size_t
+    {
+      return capacity_value;
+    }
+
+    [[nodiscard]] static constexpr auto
+    collection_maximum_element_count() noexcept -> std::size_t
+    {
+      return CLJONIC_COLLECTION_MAXIMUM_ELEMENT_COUNT;
+    }
+
+    [[nodiscard]] constexpr auto logical_size() const noexcept -> std::size_t
+    {
+      return logical_size_;
+    }
+
+    [[nodiscard]] constexpr auto state() const noexcept -> vector_state
+    {
+      const detail::vector::vector_observation observation{
+          static_cast<int>(capacity_value), static_cast<int>(logical_size_)};
+      return detail::vector::classify_vector(observation);
+    }
+
+    [[nodiscard]] constexpr auto try_push_back(const value_type &value) noexcept(
+        concepts::NothrowCopyAssignable<value_type>) -> bool
+    {
+      if (logical_size_ >= capacity_value)
+      {
+        return false;
+      }
+
+      storage_[logical_size_] = value;
+      ++logical_size_;
+      return true;
+    }
+
+  private:
+    std::array<value_type, capacity_value> storage_{};
+    std::size_t logical_size_ = 0;
+  };
+
+  template <typename First, typename... Rest>
+  Vector(First, Rest...) -> Vector<First, 1 + sizeof...(Rest)>;
+
+  template <concepts::VectorElement element_type, std::size_t capacity_value>
+  [[nodiscard]] constexpr auto
+  count(const Vector<element_type, capacity_value> &collection) noexcept
+      -> std::size_t
+  {
+    return collection.logical_size();
   }
-
-private:
-  std::array<value_type, capacity_value> storage_{};
-  std::size_t logical_size_ = 0;
-};
-
-template <typename First, typename... Rest>
-Vector(First, Rest...) -> Vector<First, 1 + sizeof...(Rest)>;
-
-template <concepts::VectorElement element_type, std::size_t capacity_value>
-[[nodiscard]] constexpr auto
-count(const Vector<element_type, capacity_value> &collection) noexcept
-    -> std::size_t {
-  return collection.logical_size();
-}
 
 } // namespace cljonic
