8c664b8572f82dc280533ebcd9902561300d301d
Paul Whittington <pwhittington@ricelake.com>
2026-08-08 19:54:53 -0600

    fix: update count function to return size instead of logical_size and improve documentation

--- src/cljonic-core-count.hpp
+++ src/cljonic-core-count.hpp
@@ -6,14 +6,27 @@ namespace cljonic {
 
 /** \anchor Core_Count
  * \b count returns the number of logical elements in \p collection.
- * Applies to all \b CopyOnModifyCollection types: \ref Vector, Set, Map, and
- * String.
+ *
+ ~~~~~{.cpp}
+ #include "cljonic-core.hpp"
+ using namespace cljonic;
+
+ int main() {
+   const auto empty = Vector<int, 4>{};
+   const auto populated = Vector{1, 2};
+
+   const auto n0 = count(empty);     // 0
+   const auto n1 = count(populated); // 2
+
+   return (n0 == 0 && n1 == 2) ? 0 : 1;
+ }
+ ~~~~~
  */
 template <concepts::VectorElement element_type, std::size_t capacity_value>
 [[nodiscard]] constexpr auto
 count(const Vector<element_type, capacity_value> &collection) noexcept
     -> std::size_t {
-  return collection.logical_size();
+  return collection.size();
 }
 
 } // namespace cljonic
