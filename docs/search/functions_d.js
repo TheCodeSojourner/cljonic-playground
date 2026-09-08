b8fa7654ef98c1d07ac9181336b06399e12dc2ca
Paul Whittington <pwhittington@ricelake.com>
2026-09-08 16:26:11 -0600

    Update docs

--- docs/search/functions_d.js
+++ docs/search/functions_d.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['seq_0',['seq',['../namespacecljonic.html#af23ad4b89cbea89172bb757a5fbd5b26',1,'cljonic']]],
   ['set_1',['set',['../classcljonic_1_1_set.html#ae3ecb3bcbd7a96dd6754306bee88741a',1,'cljonic::Set::Set()'],['../namespacecljonic.html#af20d169ef36288b54ca608a14eb6b557',1,'cljonic::Set()']]],
-  ['string_2',['string',['../classcljonic_1_1_string.html#a7ea679911d132d5b5c601904153c8d64',1,'cljonic::String::String() noexcept'],['../classcljonic_1_1_string.html#ae3e26d18bb8bc456e6acd07b28ec73b2',1,'cljonic::String::String(const char(&amp;arr)[N]) noexcept']]]
+  ['string_2',['string',['../classcljonic_1_1_string.html#a7ea679911d132d5b5c601904153c8d64',1,'cljonic::String::String() noexcept'],['../classcljonic_1_1_string.html#ae3e26d18bb8bc456e6acd07b28ec73b2',1,'cljonic::String::String(const char(&amp;arr)[N]) noexcept'],['../namespacecljonic.html#ac8f7c83a056ffb2ba64e432680d09b21',1,'cljonic::String()']]]
 ];
