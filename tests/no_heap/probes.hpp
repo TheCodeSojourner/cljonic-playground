6198f15fdf412eddf0ea1bc0a58c366b1c530f4b
Paul Whittington <pwhittington@ricelake.com>
2026-09-08 13:30:23 -0600

    Remove unused test files and probes for First, Next, Rest, and Seq operations

--- tests/no_heap/probes.hpp
+++ tests/no_heap/probes.hpp
@@ -17,18 +17,14 @@ namespace cljonic::no_heap::probes {
 [[nodiscard]] auto disj_probe() noexcept -> bool;
 [[nodiscard]] auto dissoc_probe() noexcept -> bool;
 [[nodiscard]] auto empty_probe() noexcept -> bool;
-[[nodiscard]] auto first_probe() noexcept -> bool;
 [[nodiscard]] auto get_probe() noexcept -> bool;
 [[nodiscard]] auto is_empty_probe() noexcept -> bool;
 [[nodiscard]] auto map_entry_probe() noexcept -> bool;
 [[nodiscard]] auto map_probe() noexcept -> bool;
-[[nodiscard]] auto next_probe() noexcept -> bool;
 [[nodiscard]] auto not_empty_probe() noexcept -> bool;
 [[nodiscard]] auto peek_probe() noexcept -> bool;
 [[nodiscard]] auto pop_probe() noexcept -> bool;
 [[nodiscard]] auto queue_probe() noexcept -> bool;
-[[nodiscard]] auto rest_probe() noexcept -> bool;
-[[nodiscard]] auto seq_probe() noexcept -> bool;
 [[nodiscard]] auto set_probe() noexcept -> bool;
 [[nodiscard]] auto string_probe() noexcept -> bool;
 
